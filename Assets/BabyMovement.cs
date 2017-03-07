59f796db9c1c9b873ab439d597fc7aa724a49b67
Haukur <haukur91@gmail.com>
2017-03-07 15:24:27 +0000

    baby vol 89374

--- Assets/BabyMovement.cs
+++ Assets/BabyMovement.cs
@@ -12,13 +12,14 @@ public class BabyMovement : MonoBehaviour {
 	void Start () {
 		baby = gameObject.GetComponent<Transform> ();
 		//torso = baby.FindChild ("Torso").GetComponent <Transform>();
+		anim = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (Input.GetKey (KeyCode.W)) {
 			//torso.position += Vector3.forward / 5;
-			baby.position += Vector3.forward / 5;
+			//baby.position += Vector3.forward / 5;
 			anim.SetBool("Walking", true);
 		} else {
 			anim.SetBool ("Walking", false);
